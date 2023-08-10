b07aec767aa82935beb130726abcbd4ce4754380
BindiyaValecha <bindiyavalecha4@gmail.com>
2023-08-10 19:24:40 +0500

    today update

---
+++ task.js
@@ -0,0 +1,268 @@
+// let score = 90;
+// if (score >= 80){
+//   console.log("Great job");
+// }
+// else{
+//   console.log("Keep practicing");
+// }
+
+
+let score2 = 50;
+if(score2 >= 90){
+    console.log("A");
+}
+else if (score2 >= 80 && score2 <= 89){
+console.log("B");
+}
+else if (score2>= 70 && score2 <= 79){
+console.log("C");
+}
+else if (score2 >= 60 && score2 <= 69){
+    console.log("D");
+}
+else{
+    console.log("F")
+}
+
+
+let num
+number1 = 44;
+if (number1 >= 99){
+    console.log("first number is greater");
+}
+else if(number1 >= 220){
+    console.log("second number")
+}
+
+
+
+
+
+let num1 = 22;
+let num2 = 99;
+console.log("Hello world");
+let name = "kit";
+console.log("Hello", name);
+let num3 = 44;
+let num4 = 2;
+console.log("sum", num3 + num4);
+console.log("subtraction", num3 - num4);
+console.log("multiplication", num3 * num4);
+console.log("division", num3 / num4);
+let age = 19;
+console.log("You are", age, "years old");
+let birthYear = 2004;
+console.log("You were born in", birthYear);
+let age1 = 55
+
+if (age1 >= 18) {
+    console.log("You are eligible");
+}
+else{
+    console.log("You are not eligible");
+}
+let temperature = 99;
+console.log("The temperature is", temperature, "degrees celsius");
+let rainning = true;
+if(rainning == true ){
+    console.log("it is rainning");
+}
+else{
+    console.log("it's not rainning");
+}
+
+let color = "Blue"
+if(color == "red"){
+    console.log("Red is the color of fire");
+}
+else if (color == "Green"){
+    console.log("Green is the color of nature");
+}
+else if(color == "Blue"){
+console.log("Blue is the color of sky");
+}
+else{
+    console.log("I don't know what that color means");
+}
+
+let score = 50;
+if (score >= 90){
+console.log("You got an A");
+}
+else if(score >= 80 && score <= 89 ){
+    console.log("You got a B");
+}
+else if(score >= 70 && score <= 79){
+    console.log("You a got C");
+}
+else if(score >= 60 && score <= 69){
+    console.log("You got a D");
+}
+else{
+    console.log("You failed");
+}
+
+let age4 = 20;
+if (age4 >= 18){
+    console.log("You are eligible to enter");
+}
+else{
+    console.log("Sorry, you are too young to enter");
+}
+
+
+let num8 = 44;
+let num9 = 44;
+if(num8 > num9){
+    console.log("Larger number", num8 )
+}
+else if(num9 > num8){
+    console.log("Large number",num9)
+}
+else if (num8 == num9){
+    console.log("They are equal");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let temperature1 = 20;
+if(temperature1 > 25){
+    console.log("It's very hot day");
+}
+else if (temperature1 < 10){
+    console.log("it's very cold");
+}
+else {
+    console.log("The weather is pleasant");
+}
+
+
+
+let userName = "Bindiya Valecha";
+let password = "12345";
+if(userName == "Bindiya Valecha" && password == "12345"){
+    console.log("Longing successful")
+}
+else{
+    console.log("password or username is incorrect");
+}
+
+
+let number4 = -6;
+if(number4 > 0){
+    console.log("The number is positive");
+}
+else if (number4 < 0){
+    console.log("The number is nagetive");
+}
+else if (number4 == 0){
+    console.log("The number is zero");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
