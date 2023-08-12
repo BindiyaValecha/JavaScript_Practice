14cea47aafb02246a8a0be5853ec17909a9af6aa
BindiyaValecha <bindiyavalecha4@gmail.com>
2023-08-12 19:22:25 +0500

    update

--- homework.js
+++ homework.js
@@ -125,6 +125,54 @@ else{
 
 
 
+let num1 = 22;
+let num2 = 11;
+let num3  = 88;
+if (num1 > num2 && num1 > num3){
+    console.log(num1);
+}
+else if (num2 > num1  && num2 >num3){
+    console.log(num2);
+}
+else if (num3 > num2 && num3 > num1){
+    console.log(num3);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num4 = 20;
+// let num5 = 18;
+// if (num4 > num5){
+// console.log(num4);
+// }
+// else if (num5 > num4){
+//     console.log(num5);
+// }
+// else{
+//     console.log("The number are equal");
+// }
+    
+
+
+
+
+
+
+
+
+
+
 
 
 
