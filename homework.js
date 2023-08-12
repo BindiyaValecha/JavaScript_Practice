3887bb3b2e7ea1cd1f5ac0547ab9ffbdadc2d724
BindiyaValecha <bindiyavalecha4@gmail.com>
2023-08-12 16:24:18 +0500

    update

--- homework.js
+++ homework.js
@@ -53,9 +53,14 @@ let a = [0, 29, 55, 99];
 
 
 
-
-
-
+let userName1 = "Barbei";
+let password1 = "22334";
+if (userName1 == "Barbei" && password1 == "22334"){
+    console.log("longing successful");
+}
+else{
+    console.log("The userName1 or password is incorrect" );
+}
 
 
 
