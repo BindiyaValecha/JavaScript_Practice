a4b7e56d2c14e4c4bed2f06534044006cab71285
BindiyaValecha <bindiyavalecha4@gmail.com>
2023-08-19 19:59:14 +0500

    update

--- loop_practice2.js
+++ loop_practice2.js
@@ -47,6 +47,88 @@ for(let i = 1;i <= 10; i++){
 
 
 
+let nabc =10;
+
+for(let i = 0; i <= nabc; i++){
+    console.log(i)
+}
+
+
+
+sum1 = 0;
+n = 5;
+for (let i = 1; i <=n ; i++){
+    sum1 += i; //sum1 = sum1 + i
+}
+console.log("sum1", sum1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+sum = 0
+n1 = 5
+for(let i = n1; i >= n1; i--){
+    sum +=i
+}
+console.log("sum2", sum)
+//sum = 0
+// n1 = 5
+//i = 1
+//1 >= 5?false
+
+// i = 5
+// i--
+// 
+for (let i = 5; i >= 1; i--){
+    console.log(i)
+}
+
+for(let i = 50; i <= 55; i++){
+    console.log(i)
+}
+//i = 50
+//50
+for(let i = 70; i >= 60; i--){
+    console.log(i)
+}
+
+
+
+// 105 110 115 120 125 130 ... 150
+for(let i = 105; i <= 150; i+=5){
+    console.log(i)
+}
+
+
+for(let i = 1; i <= 100; i+=2 ){
+    console.log(i)
+}
+
+
+for(let i = 0; i <= 100; i+=2)[
+    console.log(i)
+]
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
