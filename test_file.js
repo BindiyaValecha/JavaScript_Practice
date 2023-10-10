e630180c9267281bb6b6f7cba8713ed65f891994
BindiyaValecha <bindiyavalecha4@gmail.com>
2023-10-10 19:33:34 +0500

    update

--- test_file.js
+++ test_file.js
@@ -2,6 +2,14 @@ console.log("Just for testing purpose")
 let number = 110;
 number = 330;
 console.log(number * number)
+//
 let fullName = "Avinash";
 let lastName = "Kumar";
 console.log(fullName, lastName)
+//
+console.log("2 * 3 = ", 2 * 3);
+//
+let num1 = 112;
+num1 = "harry";
+console.log(num1)
+
