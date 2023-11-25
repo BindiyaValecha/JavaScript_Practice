11a8418e1883af33c180bb72744d5eb78daf5aaa
BindiyaValecha <bindiyavalecha4@gmail.com>
2023-11-25 17:00:00 +0500

    update

---
+++ practice1.js
@@ -0,0 +1,96 @@
+console.log("Rainy day")
+// 2
+let age = 19;
+age = 21;
+console.log(age, typeof age)
+// 3
+let world = "Good"// string
+console.log(world, typeof world)
+let denny = null
+console.log(denny, typeof denny)
+let very = true;
+console.log(very, typeof very)
+// 4
+let sweet = 10.1;
+console.log(sweet, typeof sweet)
+// arithmetic opperater
+let number1 = 22;
+let number2 = 11;
+console.log( "Additionm of numbers is", number1 + number2);
+console.log( "Substraction of numbers is",number1 - number2);
+console.log(  "Multiplication of numbers is" , number1 * number2);
+console.log( "Division of numbers is" ,  number1 / number2)
+console.log(5/2)
+console.log(5%2)
+console.log(7%2)
+console.log(7%4)
+let a = "hello"
+let b = "world"
+console.log(a += b)
+
+let num1 = 1;
+let num2= 2;
+num1 = num1+num2;
+
+
+num2/=num1;
+console.log(num2)
+
+// comparasion operators
+let bindiyaAge = 24;
+let avinashAge  = 23;
+let answer = bindiyaAge == avinashAge; //equal to 
+console.log(answer)
+
+answer = bindiyaAge != avinashAge;   // not equal to 
+console.log(answer)
+
+answer = bindiyaAge > avinashAge  // greater than 
+console.log(answer)
+
+answer = bindiyaAge < avinashAge   // less than
+console.log(answer)
+
+answer = bindiyaAge >= avinashAge   // greater than or equal to
+console.log(answer)
+
+answer = bindiyaAge <= avinashAge    // less than or equal to
+console.log(answer)
+
+answer = bindiyaAge === avinashAge    // equal value or equal type
+console.log(answer)
+
+answer = bindiyaAge !== avinashAge     // not equal value or not equal type
+console.log(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
